e3260372bcc3af092092dddf6aafd545bb7dae9c
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-29 22:08:26 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false
+    }
+  }
+   
   render() {
+    const {loggedIn} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+    {loggedIn? {routes}: <Login/>}
+    </div>
+    )
   }
 }
 
